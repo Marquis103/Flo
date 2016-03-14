60bbd741c0ec33a0ef3d71adb8685b3b85588a2c
Marquis Dennis <marquis103@gmail.com>
2016-03-13 23:15:49 -0400

    type: Added graphview added graphview and implemented animation to flip between graphview and counterview via uitap gesture on corresponding containerview

---
+++ Flo/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Flo
+//
+//  Created by Marquis Dennis on 3/13/16.
+//  Copyright © 2016 Marquis Dennis. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -13,10 +13,27 @@ class ViewController: UIViewController {
 
 	@IBOutlet weak var counterLabel: UILabel!
 	@IBOutlet weak var counterView: CounterView!
+	@IBOutlet weak var containerView: UIView!
+	@IBOutlet weak var graphView: GraphView!
+	
+	var isGraphShowing = false
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	
+	@IBAction func counterViewTapped(sender: UITapGestureRecognizer?) {
+		if isGraphShowing {
+			//hide graph
+			UIView.transitionFromView(graphView, toView: counterView, duration: 1.0, options: [.TransitionFlipFromLeft, .ShowHideTransitionViews], completion: nil)
+		} else {
+			//show graph
+			UIView.transitionFromView(counterView, toView: graphView, duration: 1.0, options: [.TransitionFlipFromRight, .ShowHideTransitionViews], completion: nil)
+		}
+		
+		isGraphShowing = !isGraphShowing
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -37,6 +54,11 @@ class ViewController: UIViewController {
 		}
 		
 		counterLabel.text = "\(counterView.counter)"
+		
+		//show counter view if graph is showing while add button is pressed
+		if isGraphShowing {
+			counterViewTapped(nil)
+		}
 	}
 
 }
