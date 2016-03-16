22034e7d72738ec43a8e42f4addf49fb3c227ef5
Marquis Dennis <marquis103@gmail.com>
2016-03-16 01:10:20 -0400

    type: updated graphview added labels to graphview and added transitions in counter view between graphview and counter view

--- Flo/GraphView.swift
+++ Flo/GraphView.swift
@@ -8,14 +8,124 @@
 
 import UIKit
 
+@IBDesignable
 class GraphView: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+	@IBInspectable var startColor: UIColor = UIColor.redColor()
+	@IBInspectable var endColor: UIColor = UIColor.greenColor()
+	
+	var graphPoints:[Int] = [4, 2, 6, 4, 5, 8, 3]
+	
     override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
+		let width = rect.width
+		let height = rect.height
+		
+		//set up background clipping area
+		let path = UIBezierPath(roundedRect: rect, byRoundingCorners: .AllCorners, cornerRadii: CGSize(width: 8.0, height: 8.0))
+		path.addClip()
+		
+        //get the current context
+		let context = UIGraphicsGetCurrentContext()
+		let colors = [startColor.CGColor, endColor.CGColor]
+		
+		//set up colorspace
+		let colorSpace = CGColorSpaceCreateDeviceRGB()
+		
+		//set up color stops
+		let colorLocations:[CGFloat] = [0.0, 1.0]
+		
+		//create gradient
+		let gradient = CGGradientCreateWithColors(colorSpace, colors, colorLocations)
+		
+		//draw the gradient
+		let startPoint = CGPoint.zero
+		let endPoint = CGPoint(x: 0, y: bounds.height)
+		
+		CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, .DrawsBeforeStartLocation)
+		
+		let margin:CGFloat = 20.0
+		let columnXPoint = { (column:Int) -> CGFloat in
+			//calculate gap between points
+			let spacer = (width - margin * 2 - 4) / CGFloat((self.graphPoints.count - 1))
+			var x:CGFloat = CGFloat(column) * spacer
+			x += margin + 2
+			return x
+		}
+		
+		let topBorder:CGFloat = 60
+		let bottomBorder:CGFloat = 50
+		let graphHeight = height - topBorder - bottomBorder
+		let maxValue = self.graphPoints.maxElement()!
+		
+		let columnYPoint = { (graphPoint:Int) -> CGFloat in
+			var y:CGFloat = CGFloat(graphPoint) / CGFloat(maxValue) * graphHeight
+			y = graphHeight + topBorder - y
+			return y
+		}
+		
+		UIColor.whiteColor().setFill()
+		UIColor.whiteColor().setStroke()
+		
+		let graphPath = UIBezierPath()
+		graphPath.moveToPoint(CGPoint(x: columnXPoint(0), y: columnYPoint(graphPoints[0])))
+		
+		for i in 1..<graphPoints.count {
+			let nextPoint = CGPoint(x:columnXPoint(i), y: columnYPoint(graphPoints[i]))
+			graphPath.addLineToPoint(nextPoint)
+		}
+		
+		//save the sate of the context
+		CGContextSaveGState(context)
+		
+		//make a copy of the path
+		let clippingPath = graphPath.copy() as! UIBezierPath
+		
+		//add lines to the copied path to complete the clip area
+		clippingPath.addLineToPoint(CGPoint(x: columnXPoint(graphPoints.count - 1), y: height))
+		clippingPath.addLineToPoint(CGPoint(x: columnXPoint(0), y: height))
+		clippingPath.closePath()
+		
+		//add clipping path to context
+		clippingPath.addClip()
+		
+		let highestYPoint = columnYPoint(maxValue)
+		let sPoint = CGPoint(x: margin, y: highestYPoint)
+		let ePoint = CGPoint(x: margin, y: bounds.height)
+		
+		CGContextDrawLinearGradient(context, gradient, sPoint, ePoint, .DrawsBeforeStartLocation)
+		CGContextRestoreGState(context)
+		
+		graphPath.lineWidth = 2.0
+		graphPath.stroke()
 
+		for i in 0..<graphPoints.count {
+			var point = CGPoint(x: columnXPoint(i), y: columnYPoint(graphPoints[i]))
+			point.x -= 5.0/2
+			point.y -= 5.0/2
+			
+			let circle = UIBezierPath(ovalInRect: CGRect(origin: point, size: CGSize(width: 5.0, height: 5.0)))
+			circle.fill()
+		}
+		
+		//draw horizontal lines on the top of everything
+		let linePath = UIBezierPath()
+		
+		//top line
+		linePath.moveToPoint(CGPoint(x: margin, y: topBorder))
+		linePath.addLineToPoint(CGPoint(x: width - margin, y: topBorder))
+		
+		//center line
+		linePath.moveToPoint(CGPoint(x: margin, y: graphHeight/2 + topBorder))
+		linePath.addLineToPoint(CGPoint(x: width - margin, y: graphHeight/2 + topBorder))
+		
+		//bottom line
+		linePath.moveToPoint(CGPoint(x: margin, y: height - bottomBorder))
+		linePath.addLineToPoint(CGPoint(x: width - margin, y: height - bottomBorder))
+		
+		let color = UIColor(white: 1.0, alpha: 0.3)
+		color.setStroke()
+		
+		linePath.lineWidth = 1.0
+		linePath.stroke()
+    }
 }
--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -13,14 +13,16 @@ class ViewController: UIViewController {
 
 	@IBOutlet weak var counterLabel: UILabel!
 	@IBOutlet weak var counterView: CounterView!
-	@IBOutlet weak var containerView: UIView!
+	@IBOutlet weak var containervarw: UIView!
 	@IBOutlet weak var graphView: GraphView!
+	@IBOutlet weak var averageWaterdrunk: UILabel!
+	@IBOutlet weak var maxLabel: UILabel!
 	
 	var isGraphShowing = false
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
 	}
 	
 	
@@ -30,6 +32,7 @@ class ViewController: UIViewController {
 			UIView.transitionFromView(graphView, toView: counterView, duration: 1.0, options: [.TransitionFlipFromLeft, .ShowHideTransitionViews], completion: nil)
 		} else {
 			//show graph
+			setupGraphDisplay()
 			UIView.transitionFromView(counterView, toView: graphView, duration: 1.0, options: [.TransitionFlipFromRight, .ShowHideTransitionViews], completion: nil)
 		}
 		
@@ -60,6 +63,53 @@ class ViewController: UIViewController {
 			counterViewTapped(nil)
 		}
 	}
-
+	
+	func setupGraphDisplay() {
+		
+		//Use 7 days for graph - can use any number,
+		//but labels and sample data are set up for 7 days
+		let noOfDays:Int = 7
+		
+		//replace last day with today's actual data
+		graphView.graphPoints[graphView.graphPoints.count-1] = counterView.counter
+		
+		//indicate that the graph needs to be redrawn
+		graphView.setNeedsDisplay()
+		
+		maxLabel.text = "\(graphView.graphPoints.maxElement())"
+		
+		//calculate average from graphPoints
+		let average = graphView.graphPoints.reduce(0, combine: +) / graphView.graphPoints.count
+		averageWaterdrunk.text = "\(average)"
+		
+		//set up labels
+		//day of week labels are set up in storyboard with tags
+		//today is last day of the array need to go backwards
+		
+		//get today's day number
+		let dateFormatter = NSDateFormatter()
+		let calendar = NSCalendar.currentCalendar()
+		let componentOptions:NSCalendarUnit = .NSWeekdayCalendarUnit
+		let components = calendar.components(componentOptions, fromDate: NSDate())
+		
+		var weekday = components.weekday
+		
+		let days = ["S", "S", "M", "T", "W", "T", "F"]
+		
+		//set up the day name labels with correct day
+		for i in days.count.stride(to: 1, by: -1) {
+			if let labelView = graphView.viewWithTag(i) as? UILabel {
+				if weekday == 7 {
+					weekday = 0
+				}
+				
+				labelView.text = days[weekday--]
+				
+				if weekday < 0 {
+					weekday = days.count - 1
+				}
+			}
+		}
+	}
 }
 
